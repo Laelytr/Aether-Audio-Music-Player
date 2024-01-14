11996af470678495d9be3462dd4389e7c8813531
Laely Triana <89027490+Laelytr@users.noreply.github.com>
2024-01-14 21:30:19 +0700

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,151 @@
+/*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ J S    L I S T    L A G U ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+let semualagu = [{
+    nama: "My Chemical Romance - Disenchantedㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ𝚆𝚎𝚜𝚝 𝙼𝚞𝚜𝚒𝚌ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ 4:55",
+    album: "< West Music >",
+    photo: "Image/disenchanted",
+    audio: "Music/My Chemical Romance - Disenchanted.mp3"
+}, {
+    nama: "Ghea Indrawari - Jiwa Yang Bersedihㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ𝘐𝘯𝘥𝘰𝘯𝘦𝘴𝘪𝘢𝘯 𝘔𝘶𝘴𝘪𝘤ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ 4:38",
+    album: "< Indonesian Music >",
+    photo: "Image/gheaindrawari",
+    audio: "Music/Ghea Indrawari - Jiwa Yang Bersedih.mp3"
+}, {
+    nama: "Coldplay - Viva la Vidaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ𝘞𝘦𝘴𝘵 𝘔𝘶𝘴𝘪𝘤ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ4:02",
+    album: "< West Music >",
+    photo: "Image/coldplay",
+    audio: "Music/Coldplay - Viva la Vida.mp3"
+}, {
+    nama: "NCS - Royalty ft Neoniㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ𝘕𝘰 𝘊𝘰𝘱𝘺𝘳𝘪𝘨𝘩𝘵 𝘔𝘶𝘴𝘪𝘤ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ 3:44",
+    album: "< No Copyright Music >",
+    photo: "Image/ncsroyalty",
+    audio: "Music/NCS - Royalty ft Neoni.mp3"
+}, {
+    nama: "DJ Mocil Fvnky - Karena Su Sayangㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ𝘋𝘫 𝘙𝘦𝘮𝘪𝘹ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ4:00",
+    album: "< DJ Remix >",
+    photo: "Image/mocilsusayang",
+    audio: "Music/DJ Mocil Fnvky - Karena Su Sayang.mp3"
+}, {
+    nama: "Little Mix - Secret Love Songㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ𝘞𝘦𝘴𝘵 𝘔𝘶𝘴𝘪𝘤ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ ㅤㅤㅤ ㅤㅤ ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ4:20",
+    album: "< West Music >",
+    photo: "Image/secretlovesong",
+    audio: "Music/Little Mix - Secret Love Song.mp3"
+}, {
+    nama: "Last Child - Dukaㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ 𝘐𝘯𝘥𝘰𝘯𝘦𝘴𝘪𝘢𝘯 𝘔𝘶𝘴𝘪𝘤ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ ㅤ 5:19",
+    album: "< Indonesian Music >",
+    photo: "Image/duka",
+    audio: "Music/Last Child - Duka.mp3"
+}]
+/*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ V A R    K O M P O N E N    P E M U T A R ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+var audioPlayer = document.getElementById("audioPlayer"); /*PUTAR LAGU*/
+var musicName = document.getElementById("musicName");     /*NAMA LAGU*/
+var musicArtist = document.getElementById("musicArtist"); /*ALBUM LAGU*/
+var albumPhoto = document.getElementById("albumPhoto");   /*FOTO LAGU*/
+var albumPhoto2 = document.getElementById("albumPhoto2");
+var songsList = document.getElementById("songsList");     /*LIST LAGU*/
+
+/*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ J S    D A F T A R    L A G U ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+for (var i = 0; i < semualagu.length; i++) {
+    var li = document.createElement("li");
+    li.textContent = semualagu[i].nama;
+    li.addEventListener("click", (function (index) {
+        return function () {
+            loadData(index);
+        };
+    })(i));
+    songsList.appendChild(li);
+}
+    /*FUNGSI LOAD DATA UNTUK MENAMPILKAN KOMPONEN LAGU*/
+function loadData(indexValue) {
+    var lagu = semualagu[indexValue];
+    musicName.innerHTML = lagu.nama;
+    musicArtist.innerHTML = lagu.album;
+    albumPhoto.src = lagu.photo + ".jpg";
+    albumPhoto2.src = lagu.photo + ".jpg";
+    audioPlayer.src = lagu.audio;
+
+    /*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ J S    P L A Y    L A G U ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+    audioPlayer.load();
+    audioPlayer.play();
+
+
+    
+    /*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  J S    S H O W    P L A Y    M U S I C ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+    document.getElementById("songDetails").style.display = "block";
+    toggleView();
+}
+
+/*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ J S    H I D D E N    D A F T A R    L A G U ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+function toggleView() {
+    var daftarLagu = document.querySelector(".daftar-lagu");
+    var pemutarLagu = document.querySelector(".pemutar-lagu");
+    daftarLagu.style.display = "none";
+    pemutarLagu.style.display = "block";
+}
+
+/*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ J S    T O M B O L    P L A Y    P A U S E ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+    /*FUNGSI TOMBOL PLAY DAN PAUSE RANGKAP*/
+var playPauseBtn = document.getElementById('playPauseBtn');
+
+    /*FUNGSI LOGIKA PLAY PAUSE*/
+function togglePlayPause() {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        playPauseBtn.innerHTML = '❙❙';
+    } else {
+        audioPlayer.pause();
+        playPauseBtn.innerHTML = '▶';
+    }
+}
+
+/*▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ J S    T O M B O L    S L O W    S P E E D    U P    D A N    P R E V    N E X T ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀*/
+    /*FUNGSI TOMBOL PREV DAN NEXT*/
+document.getElementById('prevBtn');
+var nextBtn = document.getElementById('nextBtn');
+var currentSong = 0;
+
+    /*FUNGSI LOGIKA PREV LAGU*/
+function prevSong() {
+    if (currentSong > 0) {
+        currentSong--;
+    } else {
+        currentSong = semualagu.length - 1;
+    }
+    /*MENAMPILKAN PREV KOMPONEN LAGU*/
+    audioPlayer.src = semualagu[currentSong].audio;
+    musicName.textContent = semualagu[currentSong].nama;
+    musicArtist.textContent = semualagu[currentSong].album;
+    albumPhoto.src = semualagu[currentSong].photo + ".jpg";
+    albumPhoto2.src = semualagu[currentSong].photo + ".jpg";
+    audioPlayer.play();
+}
+    /*FUNGSI LOGIKA PREV LAGU*/
+function nextSong() {
+    if (currentSong < semualagu.length - 1) {
+        currentSong++;
+    } else {
+        currentSong = 0;
+    }
+    /*MENAMPILKAN NEXT KOMPONEN LAGU*/
+    audioPlayer.src = semualagu[currentSong].audio;
+    musicName.textContent = semualagu[currentSong].nama;
+    musicArtist.textContent = semualagu[currentSong].album;
+    albumPhoto.src = semualagu[currentSong].photo + ".jpg";
+    albumPhoto2.src = semualagu[currentSong].photo + ".jpg";
+    audioPlayer.play();
+
+}
+    /*MELAKUKAN FUNGSI NEXTSONG APABILA AUDIOPLAYER ENDED*/
+audioPlayer.addEventListener('ended', function() {
+    nextSong();
+  });
+
+    /*FUNGSI SLOW DAN SPEED UP 0.10X*/
+  var audioPlayer = document.getElementById('audioPlayer');
+
+  function slowDown() {
+    audioPlayer.playbackRate -= 0.10;
+  }
+
+  function speedUp() {
+    audioPlayer.playbackRate += 0.10;
+}
